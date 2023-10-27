66a427ab07b77ed221638116729d5cd4a700e990
JoeJoeflyn <thaitainguyen336@gmail.com>
2023-10-27 15:55:55 +0700

    minor changes

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use("/api", (req, res) => {
+  res.json({ message: "E Commerce API" });
+});
 app.use("/api/admin", checkAdmin, adminRouter);
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
